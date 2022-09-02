d76f6584c3a3eed7a0304e1e7ce57f0d4ae12aaa
bolhy91 <bolhy91@gmail.com>
2022-09-02 01:33:27 -0500

    Detalles en el README.md

--- app/src/main/java/com/bolhy91/androidchallengeexcelsior/presentation/detail_track/DetailTrackScreen.kt
+++ app/src/main/java/com/bolhy91/androidchallengeexcelsior/presentation/detail_track/DetailTrackScreen.kt
@@ -1,12 +1,7 @@
 package com.bolhy91.androidchallengeexcelsior.presentation.detail_track
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.bolhy91.androidchallengeexcelsior.presentation.detail_track.components.BodySection
 import com.bolhy91.androidchallengeexcelsior.presentation.detail_track.components.TopBarSectionDetail
